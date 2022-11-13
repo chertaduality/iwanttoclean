c750820c98536914fd6593258fd99455ba7df905
chertaduality <cherta.duality@gmail.com>
2022-11-13 16:09:45 +0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from './contexts/BudgetsContext';
+import AddBudgetModal from './components/AddBudgetModal';
 
-function App() {
+import setShowModal from './components/AddBudgetModal';
+
+export const App = () => {
+  const Container = styled.div``;
+  const Header = styled.div`
+    background-image: url(${'https://phonoteka.org/uploads/posts/2021-04/1618632243_19-phonoteka_org-p-sinii-fon-gradient-s-fioletovim-20.jpg'});
+    height: 10rem;
+    color: #fff;
+    padding: 2rem;
+    display: flex;
+    justify-content: space-between;
+  `;
+  const Title = styled.div`
+    justify-content: start;
+    font-size: 4rem;
+    font-weight: bolder;
+  `;
+  const Menu = styled.div`
+    justify-content: end;
+  `;
+  const Button = styled.button`
+    border-radius: 10px;
+    background-color: #fff;
+    font-weight: bolder;
+    border-style: none;
+    margin: 1rem;
+    height: 3rem;
+  `;
+  const Cards = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    gap: 1rem;
+    align-items: flex-start;
+  `;
+  const Card = styled.div`
+    border-radius: 10px;
+    /* border-color: #fff; */
+    background-color: #fff;
+    filter: drop-shadow(5px 5px 0.2rem #000);
+
+    margin: 1rem;
+  `;
+  const CardTitle = styled.div`
+    background-color: #444ddd;
+    color: #ff231f;
+    font-weight: bold;
+    font-size: 1.5rem;
+    padding: 0.5rem;
+  `;
+  const CardContent = styled.div`
+    padding: 0.5rem;
+  `;
+
+  // const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  // const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+  // const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+  // const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
+  // const { budgets, getBudgetExpenses } = useBudgets();
+  // const openAddExpenseModal = (budgetId) => {
+  //   setShowAddExpenseModal(true)
+  //   setAddExpenseModalBudgetId(budgetId)
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Header>
+          <Title>iWantTo</Title>
+          <Menu>
+            <Button onClick={() => setShowModal((prev) => !prev)}>
+              Добавить группу хотелок
+            </Button>
+            <Button>Добавить хотелку</Button>
+          </Menu>
+        </Header>
+        <div>
+          <Cards>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+            <Card>
+              <CardTitle>Title</CardTitle>
+              <CardContent>some content</CardContent>
+            </Card>
+          </Cards>
+        </div>
+      </Container>
+      {/* <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={() => setShowAddBudgetModal(false)}
+      /> */}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,76 @@
+import { useRef, useState } from 'react';
+import { useBudgets } from '../contexts/BudgetsContext';
+import styled from 'styled-components';
+import { Button } from './../App';
+import CloseModal from './CloseModal';
+
+export default function AddBudgetModal({ show, handleClose }) {
+  //   const nameRef = useRef();
+  //   const maxRef = useRef();
+  //   const { addBudget } = useBudgets();
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     addBudget({
+  //       name: nameRef.current.value,
+  //       // max: parseFloat(maxRef.current.value),
+  //     });
+  //     handleClose();
+  //   };
+  const [showModal, setShowModal] = useState(false);
+  //   const Container = styled.div``;
+  const Modal = styled.div`
+    padding: 4px;
+    display: flex;
+    flex-direction: row-reverse;
+    width: 80%;
+    height: 80vh;
+    border: 1px solid black;
+  `;
+  const Form = styled.form`
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+  `;
+
+  return (
+    <div>
+      {showModal === true ? (
+        <Modal>
+          <CloseModal setShowModal={setShowModal} />
+          <div>TEST</div>
+          <Form>TEST</Form>
+        </Modal>
+      ) : (
+        <>
+          <h4>Click the button to show the modal.</h4>
+          <button onClick={() => setShowModal((prev) => !prev)}>Show</button>
+        </>
+      )}
+
+      {/* <Modal.Header closeButton>
+    <Modal.Title>Новая група хотелок</Modal.Title>
+  </Modal.Header>
+  <Modal.Body>
+    <Form.Group className="mb-3" controlId="name">
+      <Form.Label>Название</Form.Label>
+      <Form.Control ref={nameRef} type="text" required />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="max">
+      <Form.Label>Расходы</Form.Label>
+      <Form.Control
+        ref={maxRef}
+        type="number"
+        required
+        min={0}
+        step={0.01}
+      />
+    </Form.Group>
+    <div className="d-flex justify-content-end">
+      <Button variant="primary" type="submit">
+        Добавить
+      </Button>
+    </div>
+  </Modal.Body> */}
+    </div>
+  );
+}
